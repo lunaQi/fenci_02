e43e1c48cf92363d6cacf684004153ffb861ea63
Luna <1363781483@qq.com>
2017-11-13 16:03:27 +0800

    修改分词格式并统计词频

--- fenci_02.py
+++ fenci_02.py
@@ -3,11 +3,13 @@
 import jieba
 import jieba.posseg as pseg
 
+'''
 def delete_space(stopwordspath):
 
     stw_list = [line.strip()
                 for line in open(stopwordspath,'r')]
     return stw_list
+'''
 
 f1 = open("150518it23974.txt")
 f2 = open("fenci_150518it23974.txt", 'w')
@@ -21,13 +23,15 @@ text = ''
 
 
 for line in lines:
-    line.replace('\t','').replace('\n','').replace(' ','')
-    #seg_list = jieba.cut(line, cut_all=False)
-    words = pseg.cut(line)
-    #for word in seg_list:
-    for word, flag in words:
-        if (flag == 'Ng') or (flag=='n') or (flag=='nr') or (flag=='ns') or (flag=='nt') or (flag=='nz') or (flag=='vn'):
-            text = text + '\n'+ str(word.encode('utf8')) + ' ' + str(flag)
+    line.replace('\t', '').replace('\n', '').replace(' ', '')
+    if len(line) > 0:      # delete a null line
+        #seg_list = jieba.cut(line, cut_all=False)
+        words = pseg.cut(line)
+
+        #for word in seg_list:
+        for word, flag in words:
+            if (flag == 'Ng') or (flag == 'n') or (flag == 'nr') or (flag == 'ns') or (flag == 'nt') or (flag == 'nz') or (flag == 'vn'):
+                text = text + ',' + str(word.encode('utf8'))
 
 #print(text)
 
@@ -35,3 +39,19 @@ f2.write(text)
 
 f1.close()
 f2.close()
+
+word_lst = []
+word_dict = {}
+with open("fenci_150518it23974.txt") as wf, open("wfr_150518it23974.txt", 'w') as wf2:
+    for word in wf:
+        word_lst.append(word.split(','))
+        for item in word_lst:
+            for item2 in item:
+                if item2 not in word_dict:
+                    word_dict[item2] = 1
+                else:
+                    word_dict[item2] += 1
+    for key in word_dict:
+        print key,str(word_dict[key])
+        wf2.write(key + ' ' + str(word_dict[key]) + '\n')
+
